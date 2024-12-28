9ef4c12251a2477b90caf742d8be693a83beba9b
Beram <giberam99@gmail.com>
2024-12-28 21:23:06 +0100

    aboutview fini

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,7 +4,7 @@
     <p>
       C'est deuxième site, c'est un site avec un quiz avec différents types de questions. Des QCM, QCU et question réponse.
       Les questions sont principalement des questions sur les jeux vidéos qui font partis de mes jeux favoris. Il existe la section Trivia avec des questions sur différents sujets qui proviennent de opentdb.
-      Ce site a été crée pour la validation du module d'informatique : BS21INF5 - Programmation Web et bases de données, pour le premier semestre de ma 2ème année à la HEP. Les critères sur le site et le code étaient les suivants.
+      Ce site a été crée pour la validation du module d'informatique : BS21INF5 - Programmation Web et bases de données, pour le premier semestre de ma 2ème année à la HEP. 
     </p>
     <img
     src="@/assets/dab479331cca130ce4a0f82f86dc3f00.png"
